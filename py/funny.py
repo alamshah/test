673830439bc16ee203bfed227e4a5c42f8e82b28
unknown <md shahnawaz alam>
2019-02-23 10:59:02 +0530

    modification

--- py/funny.py
+++ py/funny.py
@@ -3,12 +3,10 @@ hungry="yes"
 if hungry=="yes":
 	print("eat samosa")
 	print("eat pizza")
-<<<<<<< HEAD
 	print("eat fries")
 	print("burger")
-=======
 	print('eat burger')
->>>>>>> 1a7dea01fd7557afc2b0b9c9605e8fab3e2ff683
+
 else:
 	thirsty = input("are you thirsty?")
 	if thirsty == "yes":
