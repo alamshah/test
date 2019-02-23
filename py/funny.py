8b5cc5119eed81593b12c0ba87a70212501eff6c
md shahnawaz alam <shahnawazlm455@gmail.com>
2019-02-23 16:28:15 +0530

    added yup

--- py/funny.py
+++ py/funny.py
@@ -1,11 +1,11 @@
 print("i'm hungry")
 hungry="yes"
-if hungry=="yes" and hungry=="ya":
+if hungry=="yes" and hungry=="yup":
 	print("eat samosa")
 	print("eat pizza")
 	print("eat fries")
 else:
-	thirsty = input("are you thirsty?") 
+	thirsty = input("are you thirsty?")
 	if thirsty == "yes":
 		print("do homework")
 		print("drink water ")
